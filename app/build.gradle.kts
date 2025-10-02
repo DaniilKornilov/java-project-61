410384f7068110c69c48b7112f8453eeda084972
Daniil Kornilov <daniil-kornilov@yandex.ru>
2025-10-02 23:34:38 +0300

    add checkstyle plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("application")
+    id("checkstyle")
     id("com.github.ben-manes.versions") version "0.53.0"
 }
 
@@ -14,6 +15,14 @@ java {
 
 application {
     mainClass.set("hexlet.code.App")
+}
+
+checkstyle {
+    // Use the version of Checkstyle you want
+    toolVersion = "11.1.0"
+
+    // Optional: point to your custom config
+    configFile = file("$rootDir/config/checkstyle.xml")
 }
 
 repositories {
