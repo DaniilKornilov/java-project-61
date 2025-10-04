bb764710c91d6898d7fed79b62eaede4164977d7
Daniil Kornilov <daniil-kornilov@yandex.ru>
2025-10-04 14:39:59 +0300

    add private constructors for RandomUtils

--- app/src/main/java/hexlet/code/RandomUtils.java
+++ app/src/main/java/hexlet/code/RandomUtils.java
@@ -4,8 +4,10 @@ import java.util.Random;
 
 public class RandomUtils {
     private static final Random random = new Random();
-
     private static final int NUMBER_OF_OPERATIONS = 3;
+
+    private RandomUtils() {
+    }
 
     public static int generateNumber(int boundExclusive) {
         return random.nextInt(boundExclusive);
